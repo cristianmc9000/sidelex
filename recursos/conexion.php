bc00b091a26db15d3682b292450bd97be8bf8df6
cristianmc9000 <cristian.preof1@gmail.com>
2021-08-10 19:01:38 -0400

    probando git

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,7 +1,7 @@
 <?php 
 
 //$conexion = mysqli_connect('localhost','root','','dbpl');
-$conexion = mysqli_connect('localhost','u604223767_cristian ','Desconocido1','u604223767_dppl ');
+$conexion = mysqli_connect('localhost','u604223767_cristian ','Desconocido1','u604223767_dppl '); //agregado desde sublime
 
 if($conexion === false) { 
  echo 'Ha habido un error <br>'.mysqli_connect_error(); 
