285341f3a337ad27e2fd4a4806f8cfa73f262b52
cristianmc9000 <cristian.preof1@gmail.com>
2021-08-10 19:09:44 -0400

    cambios conexion

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,9 +1,10 @@
 <?php 
 
 //$conexion = mysqli_connect('localhost','root','','dbpl');
-$conexion = mysqli_connect('localhost','u604223767_cristian ','Desconocido1','u604223767_dppl '); //agregado desde sublime
+$conexion = new mysqli('localhost','u604223767_cristian ','Desconocido1','u604223767_dbpl'); //agregado desde sublime
 
-if($conexion === false) { 
- echo 'Ha habido un error <br>'.mysqli_connect_error(); 
+$conexion->query("SET NAMES 'utf8'");
+if($conexion->connect_error) { 
+    die( 'Error: ('. $conexion->connect_errno .' )'. $conexion->connect_error); 
 }
 ?>
