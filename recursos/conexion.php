d8832150e7d0029399ce8a096cd1654d19fd2b82
cristianmc9000 <cristian.preof1@gmail.com>
2021-08-10 18:58:47 -0400

    base de datos hostinger

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,6 +1,7 @@
 <?php 
 
-$conexion = mysqli_connect('localhost','root','','dbpl');
+//$conexion = mysqli_connect('localhost','root','','dbpl');
+$conexion = mysqli_connect('localhost','u604223767_cristian ','Desconocido1','u604223767_dppl ');
 
 if($conexion === false) { 
  echo 'Ha habido un error <br>'.mysqli_connect_error(); 
