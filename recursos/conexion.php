24767bcb30611fbaa31e3a3c398acb0861784d81
cristianmc9000 <cristian.preof1@gmail.com>
2021-09-23 17:35:17 -0400

    index.css

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,8 +1,8 @@
 <?php 
 
-$conexion = mysqli_connect('localhost','root','','dbpl');
+// $conexion = mysqli_connect('localhost','root','','dbpl');
 //$conexion = new mysqli('localhost','u604223767_cristian','Desconocido1','u604223767_dppl'); //agregado desde sublime
-// $conexion = mysqli_connect('localhost','u604223767_cristian','Desconocido1','u604223767_dppl');
+$conexion = mysqli_connect('localhost','u604223767_cristian','Desconocido1','u604223767_dppl');
 
 $conexion->query("SET NAMES 'utf8'");
 if($conexion->connect_error) { 
